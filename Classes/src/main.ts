15d856fe131f9242b917eadcffebe97fe265b380
abdulqayoom77 <abdulqayoomaijaz77@gmail.com>
2025-10-15 21:14:04 +0500

    Refactor BiscuitBananeKiRecipe class: format console log and adjust instance creation

--- Classes/src/main.ts
+++ Classes/src/main.ts
@@ -1,20 +1,23 @@
 class BiscuitBananeKiRecipe {
-//   name: string;
-//   weight: string;
+  //   name: string;
+  //   weight: string;
 
   constructor(public name: string, public weight: string) {
     this.name = name;
     this.weight = weight;
   }
   eat() {
-    console.log(`jis me dam hai kam khae ${this.name} biscuit jisme hai dam ${this.weight} jitna. `);
+    console.log(
+      `jis me dam hai kam khae ${this.name} biscuit jisme hai dam ${this.weight} jitna. `
+    );
   }
-
 }
 
 const p1 = new BiscuitBananeKiRecipe("Tiger", "23gram");
 const p2 = new BiscuitBananeKiRecipe("Super", "23gram");
 const p3 = new BiscuitBananeKiRecipe("Super", "23gram");
+const p4 = new BiscuitBananeKiRecipe("Super", "23gram");
 p1.eat();
-p2.eat();
 p3.eat();
+p4.eat();
+p2.eat();
